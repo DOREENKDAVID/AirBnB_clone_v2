6810b5d7a642201417a70ce541cac68aaa1263ae
James Kariuki <jamexkarix583@gmail.com>
2023-08-21 13:32:18 -0700

    test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
---Create a database called 
+-- Creates a database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
---Create the Mysql user
-CREATE USER IF NOT EXISTS'hbnb_test'@'localhost'IDENTIFIED BY'hbnb_test_pwd';
---We give out the permissions
-GRANT ALL ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
+--Create the user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+--Permissions 
+GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
