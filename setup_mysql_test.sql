61958a674705a651cd27186c39cf18c1da7ad28b
James Kariuki <jamexkarix583@gmail.com>
2023-08-22 01:54:58 -0700

    Updated

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,6 +2,10 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 --Create the user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
---Permissions 
-GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+--Granting the SELECT privilage for the user to the performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+-- Granting all privileges to the new User on the database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+
