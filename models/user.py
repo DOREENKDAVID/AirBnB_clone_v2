9f3aa1e04d6652bd5d34062df0df7b3fce9e96ae
James Kariuki <jamexkarix583@gmail.com>
2023-08-22 10:36:11 -0700

    Updating the file user.py

--- models/user.py
+++ models/user.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-<<<<<<< HEAD
-
-
-from models.base_model import BaseModel, Base)
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-
-
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nulluble=True)
-    last_name= Column(String(128), nullable=True)
-    
-    BaseModel = relationship("BaseModel", back_populates="users")
-=======
 from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String
@@ -42,4 +23,3 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
->>>>>>> 5521a241e9ba02e7e9ded45bec2079971360a925
