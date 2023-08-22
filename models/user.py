a9935f232281b64608c60b8777d78edabd93308f
DOREENKDAVID <doreenkinya153@gmail.com>
2023-08-22 07:50:17 -0700

    update on user and place classes

--- models/user.py
+++ models/user.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
 
 
-class User(BaseModel):
+from models.base_model import BaseModel, Base)
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+
+
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nulluble=True)
+    last_name= Column(String(128), nullable=True)
+    
+    BaseModel = relationship("BaseModel", back_populates="users")
