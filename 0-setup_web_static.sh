0852432885cac5fda7f4c870388926673909f60d
DOREENKDAVID <doreenkinya153@gmail.com>
2023-09-07 09:40:26 -0700

    script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #script that sets up your web servers for the deployment of web_static
-apt get -y update
-apt get install -y nginx
+apt-get -y update
+apt-get install -y nginx
+/etc/init.d/nginx start
 
 # create the following direcories and files
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
+
 echo "Hello configuration successful" > /data/web_static/releases/test/index.html
 
 # Check if /data/web_static/current exists and remove it
@@ -20,11 +21,28 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # give ownership of /data to ubuntu user and group
 
-chown -hR ubuntu:ubuntu /data
+chown ubuntu:ubuntu -R /data
 
 #Update the Nginx configuration to serve the content
 
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+file=/etc/nginx/sites-available/default
+rm $file
+echo -e "server {
+    listen 80 default_server;
+    root /usr/share/nginx/html;
+    index index.html index.htm;
+    add_header X-Served-By $HOSTNAME;
+    location /redirect_me {
+        return 301 https://www.youtube.com;
+    }
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+}" > $file
+
+# Test Nginx configuration and reload Nginx to apply changes
+nginx -t
+service nginx reload
 
 #restart Nginx after updating the configuration:
 service nginx restart
