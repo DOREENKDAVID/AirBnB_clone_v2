668052a55ad7df5b12d5d9df01ecda898fba7ad0
DOREENKDAVID <doreenkinya153@gmail.com>
2023-09-07 12:37:14 -0700

    Fabric scriptthat distributes an archive to your web servers, using the function do_deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Fabric script that distributes an archive to your web servers
+"""
+from fabric.operations import local, run, put
+from datetime import datetime
+from fabric.api import env
+import os
+import re
+
+
+env.hosts = ['100.25.104.17', '35.174.205.159']
+env.user = "ubuntu"
+
+def do_deploy(archive_path):
+    """ deploys web_static.tgz to the web servers
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    result = put(archive_path, "/tmp")
+    if result.failed:
+        return False
+
+    rex = r'^versions/(\S+).tgz'
+    match = re.search(rex, archive_path)
+    filename = match.group(1)
+    result = put(archive_path, "/tmp/{}.tgz".format(filename))
+    if result.failed:
+        return False
+    result = run("mkdir -p /data/web_static/releases/{}/".format(filename))
+    if result.failed:
+        return False
+    result = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+                 .format(filename, filename))
+    if result.failed:
+        return False
+    result = run("rm /tmp/{}.tgz".format(filename))
+    if result.failed:
+        return False
+    result = run("mv /data/web_static/releases/{}"
+                 "/web_static/* /data/web_static/releases/{}/"
+                 .format(filename, filename))
+    if result.failed:
+        return False
+    result = run("rm -rf /data/web_static/releases/{}/web_static"
+                 .format(filename))
+    if result.failed:
+        return False
+    result = run("rm -rf /data/web_static/current")
+    if result.failed:
+        return False
+    result = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+                 .format(filename))
+    if result.failed:
+        return False
+
+    print('New version deployed!')
+
+    return True
