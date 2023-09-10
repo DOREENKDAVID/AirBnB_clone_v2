a752e49971aba74ba27b22a4c5f5050acaad86ef
DOREENKDAVID <doreenkinya153@gmail.com>
2023-09-10 14:06:46 -0700

    puppet setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,76 +1,87 @@
-#setting up the web server and deploying web_static server_setup 
+# puppet web static set up
+# Nginx configuration file
 $nginx_conf = "server {
     listen 80 default_server;
-    root /usr/share/nginx/html;
-    index index.html index.htm;
-    add_header X-Served-By 269747-web-01;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
     location /redirect_me {
         return 301 https://github.com/DOREENKDAVID;
     }
-    location /hbnb_static {
-        alias /data/web_static/current;
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
     }
 }"
 
-  # Update package repositories and install nginx
 package { 'nginx':
-    ensure => 'installed',
-}
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
-  # Ensure that nginx service is running and enabled
-service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx'],
-}
+file { '/data':
+  ensure  => 'directory'
+} ->
 
-  # Create directories and files
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
 file { '/data/web_static/shared':
-    ensure => 'directory',
-    owner  => 'ubuntu',
-    group  => 'ubuntu',
-    mode   => '0755',
-}
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test/index.html':
-    ensure  => 'file',
-    content => "Hello configuration successful\n",
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    mode    => '0644',
-}
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
 
-  # Create symbolic link
 file { '/data/web_static/current':
-    ensure => 'link',
-    target => '/data/web_static/releases/test',
-    owner  => 'ubuntu',
-    group  => 'ubuntu',
-}
-exec { 'chown -Rubuntu:ubuntu /data/':
-    path =>'/usr/bin/:/usr/local/bin/:/bin/'
-	
-}
-file { '/var/www/html':
-    ensure => 'directory'
-}
-file { '/var/www/html/index.html':
-    ensure  => 'present',
-    content => "Hello configuration successful\n"
-}
-file { '/var/www/html/404.html':
-    ensure  => 'present',
-    content => "Ceci n'est pas une page\n"
-}
-  # Configure Nginx
-file { '/etc/nginx/sites-available/default':
-    ensure  => 'file',
-    content => $nginx_conf,
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
 
-  # Ensure Nginx is restarted after the configuration change
-exec { 'nginx_restart':
-    path => '/etc/init.d/'
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
