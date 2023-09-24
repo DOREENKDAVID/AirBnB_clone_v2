78db01667f0250d08b3a1d05b0087d2e52e3fd48
DOREENKDAVID <doreenkinya153@gmail.com>
2023-09-24 03:15:30 -0700

    fixing All html tags must be in uppercase

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,5 +69,5 @@ class FileStorage:
             del self.__objects[obj_key]
 
     def close(self):
-        """Call the reload method"""
+        """Call the reload methoderializing JSON"""
         self.reload()
