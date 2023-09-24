3352cef63c7fa2e49c1435dbc68250590309ca8d
DOREENKDAVID <doreenkinya153@gmail.com>
2023-09-24 04:00:49 -0700

    A script that starts a Flask web application nd prints HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 def display_hello():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def display_hbnb():
     return "HBNB"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
